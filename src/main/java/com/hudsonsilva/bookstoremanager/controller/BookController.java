017892f42cb06a2de161d0fd090e570ead47249d
hudson <hudsonsilva0987@gmail.com>
2021-05-27 17:36:07 -0300

    Message Reponse and controller created

--- src/main/java/com/hudsonsilva/bookstoremanager/controller/BookController.java
+++ src/main/java/com/hudsonsilva/bookstoremanager/controller/BookController.java
@@ -1,5 +1,11 @@
 package com.hudsonsilva.bookstoremanager.controller;
 
+import com.hudsonsilva.bookstoremanager.BookRepository;
+import com.hudsonsilva.bookstoremanager.dto.MessageResponseDTO;
+import com.hudsonsilva.bookstoremanager.entity.Book;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,5 +13,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/books")
 public class BookController {
 
+    private BookRepository bookRepository;
+
+    @Autowired
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @PostMapping // Anotação de criação
+    public MessageResponseDTO create(@RequestBody Book book){
+       Book savedBook = bookRepository.save(book); // FAZ A CRIAÇÃO DE UM LIVRO
+
+        return MessageResponseDTO.builder()
+                .message("Book created with ID" + savedBook.getId())
+                .build();
+    }
 
 }
