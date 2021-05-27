c4402076d93102040813f60e7a7f4782889dc9ee
hudson <hudsonsilva0987@gmail.com>
2021-05-27 16:21:59 -0300

    created BookController class

---
+++ src/main/java/com/hudsonsilva/bookstoremanager/controller/BookController.java
@@ -0,0 +1,11 @@
+package com.hudsonsilva.bookstoremanager.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // Indica que a classe representa um controller
+@RequestMapping("/api/v1/books")
+public class BookController {
+
+
+}
